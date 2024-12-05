cb1df926afbaf60b4015e854ea8eb33160787b59
camilazci <camilazucchi@outlook.com>
2024-12-05 13:14:28 -0300

    feat: add example of structured programming with book management

---
+++ src/ProgramacaoEstruturada.java
@@ -0,0 +1,14 @@
+public class ProgramacaoEstruturada {
+    public static void main(String[] args) {
+        System.out.println("- Exemplo de Programação Estruturada -\n");
+        // Código para criar e gerenciar um livro
+        String tituloLivro1 = "Verity";
+        String autorLivro1 = "Colleen Hoover";
+        System.out.println("Título livro 1: " + tituloLivro1 + "\nAutor livro 1: " + autorLivro1);
+
+        // Código duplicado para criar e gerenciar outro livro
+        String tituloLivro2 = "O Pequeno Príncipe";
+        String autorLivro2 = "Antoine de Saint-Exupéry";
+        System.out.println("\nTítulo livro 2: " + tituloLivro2 + "\nAutor livro 2: " + autorLivro2);
+    }
+}
